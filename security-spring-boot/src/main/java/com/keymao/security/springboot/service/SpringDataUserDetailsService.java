ebcd0a12d26d4116ea4a8b4b707ff30d73769b76
1044096131@qq.com <xiaojianming1220>
2020-06-16 01:42:20 +0800

    userDetailsService改为数据库认证

--- security-spring-boot/src/main/java/com/keymao/security/springboot/service/SpringDataUserDetailsService.java
+++ security-spring-boot/src/main/java/com/keymao/security/springboot/service/SpringDataUserDetailsService.java
@@ -1,5 +1,8 @@
 package com.keymao.security.springboot.service;
 
+import com.keymao.security.springboot.dao.UserDao;
+import com.keymao.security.springboot.model.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,25 +15,21 @@ import java.util.List;
 @Service
 public class SpringDataUserDetailsService implements UserDetailsService {
 
-
+    @Autowired
+    UserDao userDao;
 
     //根据 账号查询用户信息
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-/*        //将来连接数据库根据账号查询用户信息
+        //将来连接数据库根据账号查询用户信息
         UserDto userDto = userDao.getUserByUsername(username);
         if(userDto == null){
             //如果用户查不到，返回null，由provider来抛出异常
             return null;
         }
-        //根据用户的id查询用户的权限
-        List<String> permissions = userDao.findPermissionsByUserId(userDto.getId());
-        //将permissions转成数组
-        String[] permissionArray = new String[permissions.size()];
-        permissions.toArray(permissionArray);
-        UserDetails userDetails = User.withUsername(userDto.getUsername()).password(userDto.getPassword()).authorities(permissionArray).build();*/
-        UserDetails userDetails = User.withUsername("zhangsan").password("$2a$10$aFsOFzujtPCnUCUKcozsHux0rQ/3faAHGFSVb9Y.B1ntpmEhjRtru").authorities("p1").build();
+        UserDetails userDetails = User.withUsername(userDto.getUsername()).password(userDto.getPassword()).authorities("p1").build();
+        //UserDetails userDetails = User.withUsername("zhangsan").password("$2a$10$aFsOFzujtPCnUCUKcozsHux0rQ/3faAHGFSVb9Y.B1ntpmEhjRtru").authorities("p1").build();
         return userDetails;
     }
 }
