09831f03894768a633d20de2e7195148544b944b
1044096131@qq.com <xiaojianming1220>
2020-06-16 01:45:03 +0800

    新增初始化脚本

---
+++ security-spring-boot/doc/sql/init.sql
@@ -0,0 +1,12 @@
+--创建数据库
+CREATE DATABASE `user_db` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+
+--创建用户表
+CREATE TABLE `t_user` (
+`id` bigint(20) NOT NULL COMMENT '用户id',
+`username` varchar(64) NOT NULL,
+`password` varchar(64) NOT NULL,
+`fullname` varchar(255) NOT NULL COMMENT '用户姓名',
+`mobile` varchar(11) DEFAULT NULL COMMENT '手机号',
+PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
