ca3dc4afea25a3b98f67fa8558f5d18b54ef5d71
1044096131@qq.com <xiaojianming1220>
2020-06-18 01:32:06 +0800

    新增自定义退出

--- security-spring-boot/src/main/java/com/keymao/security/springboot/config/WebSecurityConfig.java
+++ security-spring-boot/src/main/java/com/keymao/security/springboot/config/WebSecurityConfig.java
@@ -46,7 +46,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()//允许表单登录
                 .loginPage("/login-view")//自定义登录页面,spring security以重定向的方式跳转到/login-view
                 .loginProcessingUrl("/login")//指定登录处理的URL，也就是用户名密码表单提交的目的路径
-                .successForwardUrl("/login-success");//自定义登录成功的页面地址
+                .successForwardUrl("/login-success")//自定义登录成功的页面地址
+                .and()
+                .logout() //自定义退出
+                .logoutUrl("/logout") //设置触发退出操作的URL
+                .logoutSuccessUrl("/login-view");//退出之后跳转的URL
 
     }
 }
