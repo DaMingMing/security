c2a5eb86509073ec0e70bbced8d824daa2d02851
DaMingMing <1044096131@qq.com>
2020-07-03 01:47:00 +0800

    授权服务配置类

---
+++ distributed-security/distributed-security-uaa/src/main/java/com/keymao/security/distributed/uaa/config/AuthorizationServer.java
@@ -0,0 +1,104 @@
+package com.keymao.security.distributed.uaa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.InMemoryAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+/**
+ * 授权服务配置类
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationCodeServices authorizationCodeServices;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    /**
+     * 配置客户端详情服务（ClientDetailsService），客户端详情信息在
+     * 这里进行初始化，能够把客户端详情信息写死在这里或者是通过数据库来存储调取详情信息
+     * @param clients
+     * @throws Exception
+     */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients)
+            throws Exception {
+        // 暂时使用内存方式
+        clients.inMemory()// 使用in‐memory存储
+            .withClient("c1")// client_id
+            .secret(new BCryptPasswordEncoder().encode("secret"))  //客户端密钥
+            .resourceIds("res1") //资料列表
+            .authorizedGrantTypes("authorization_code", "password","client_credentials","implicit","refresh_token")// 该client允许的授权类型
+            .scopes("all")// 允许的授权范围
+            .autoApprove(false)  //跳转到授权页面
+            //加上验证回调地址
+            .redirectUris("http://www.baidu.com");
+    }
+
+    /**
+     * 配置令牌访问服务
+     * @return
+     */
+    @Bean
+    public AuthorizationServerTokenServices tokenService() {
+        DefaultTokenServices service = new DefaultTokenServices();
+        service.setClientDetailsService(clientDetailsService);  //客户端信息服务
+        service.setSupportRefreshToken(true);  //是否产生刷新令牌
+        service.setTokenStore(tokenStore); //令牌存储策略
+        service.setAccessTokenValiditySeconds(7200); // 令牌默认有效期2小时
+        service.setRefreshTokenValiditySeconds(259200); // 刷新令牌默认有效期3天
+        return service;
+    }
+
+    /**
+     * 配置令牌访问端点
+     * @param endpoints
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints
+                .authenticationManager(authenticationManager) //密码模式需要
+                .authorizationCodeServices(authorizationCodeServices) //授权码模式需要
+                .tokenServices(tokenService())  //令牌管理服务
+                .allowedTokenEndpointRequestMethods(HttpMethod.POST);  //允许post提交
+    }
+
+    /**
+     * 令牌访问端点安全策略
+     * @param security
+     */
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security){
+        security
+                .tokenKeyAccess("permitAll()")  // /oauth/token_key 公开
+                .checkTokenAccess("permitAll()") //checkToken这个endpoint完全公开  /oauth/check_token
+                .allowFormAuthenticationForClients(); //允许表单认证
+    }
+/*    @Bean
+    public AuthorizationCodeServices authorizationCodeServices() { //设置授权码模式的授权码如何存取，暂时采用内存方式
+        return new InMemoryAuthorizationCodeServices();
+    }*/
+}
